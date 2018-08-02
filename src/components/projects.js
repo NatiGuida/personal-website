f3589ee325e0ef517f34ec1791c129a048c905fb
Nati <[natiguida@gmail.com]>
2018-08-02 13:34:48 -0300

    edit projects

--- src/components/projects.js
+++ src/components/projects.js
@@ -9,10 +9,10 @@ class Projects extends Component {
       <section className="projects-container container">
         <span className="projects-title">-Projects-</span>
         <div className="container-web-projects">
-          <a className="web-top"  href="https://tanintensa.com" target="_blank">
+          <a className="web-top" href="https://tanintensa.com" target="_blank">
             <img className="web" src={Webtanintensa} alt="Web Tan Intensa" />
           </a>
-          <a className="web-bot"  href="https://test.regalandolo.com" target="_blank">
+          <a className="web-bot" target="_blank">
             <img className="web" src={Webregalandolo} alt="Web Regalandolo" />
           </a>
         </div>
